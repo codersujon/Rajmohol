59860c1210629964962c9c840e1bbd1b8cf20451
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-06-29 21:00:20 +0600

    MenuItem

--- src/Components/Body/MenuItem.jsx
+++ src/Components/Body/MenuItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, CardSubtitle, Button, CardOverlay } from 'reactstrap';
+
 
 function MenuItem(props) {
-    console.log(props);
     return (
         <div>
-            
+            <Card style={{margin: "10px"}}>
+                <CardBody>
+                    <CardImg width="100%" alt={props.dish.name} src={props.dish.image} style={{opacity: "0.5"}} >
+                    </CardImg>
+                    <CardImgOverlay>
+                        <CardTitle style={{cursor: "pointer"}}>{props.dish.name}</CardTitle>
+                    </CardImgOverlay> 
+                </CardBody>
+            </Card>
         </div>
     );
 }
