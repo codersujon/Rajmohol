800e0bcc1f6717f6181689b112e8a7e8b971d695
Md. Sujan Ahmed <shobuj2sujon@gmail.com>
2022-06-29 23:10:35 +0600

    Comment show

--- src/Components/Body/DishDetail.jsx
+++ src/Components/Body/DishDetail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, CardSubtitle, Button, CardOverlay } from 'reactstrap';
+import LoadComments from './LoadComments';
 
 function DishDetail(props) {
 
@@ -13,6 +14,9 @@ function DishDetail(props) {
                     </CardTitle>
                     <CardText>{props.dish.description}</CardText>
                     <CardText><strong>{props.dish.price}/-</strong></CardText>
+                    <hr />
+                    <LoadComments comments={props.dish.comments}/>
+
                 </CardBody>
             </Card>
         </div>
